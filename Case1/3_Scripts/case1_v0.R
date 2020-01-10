14f21e182d021badc5141de1d7d0fa2237f821d2
jmontalvo94 <50923223+jmontalvo94@users.noreply.github.com>
2020-01-10 22:26:06 +0100

    Case 1 script

---
+++ Case1/3_Scripts/case1_v0.R
@@ -0,0 +1,10 @@
+#########################################################################
+## Case 1:                                                             ##
+## Effect of hardness and detergent on enzymatic catalysis             ##
+#########################################################################
+
+# Authors: Begoña Bolos Sierra, Laura Sans Comerma, Jorge Montalvo Arvizu
+
+
+# Load data
+df <- read.table("~/Github/02441_Applied_Statistics/Case1/2_Data/SPR.txt", header = TRUE, sep="\t")
