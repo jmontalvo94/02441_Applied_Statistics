9070c55c7de8177a54bbfb026c0a6a73db85816c
Laura Sans <laurasansc@gmail.com>
2020-01-13 16:16:53 +0100

    final edits for graphs and normality check

--- Case1/3_Scripts/case1_v1_L.R
+++ Case1/3_Scripts/case1_v1_L.R
@@ -13,8 +13,8 @@ require("xtable")
 #require("ggplot2") Creo q no lo  usamos al final
 
 # Load data and clean
-data <- read.table("~/Github/02441_Applied_Statistics/Case1/2_Data/SPR.txt", header = TRUE, sep="\t")
-data <- data[,-c(1,2)]
+data_raw<- read.table("~/Github/02441_Applied_Statistics/Case1/2_Data/SPR.txt", header = TRUE, sep="\t")
+data <- data_raw[,-c(1,2)]
 
 # Alternative: copy dataframe and merge detergent with hardness
 df <- data
@@ -28,16 +28,16 @@ setwd("~/Github/02441_Applied_Statistics/Case1/4_Images")
 # Testing
 y <- sort(unique(data$EnzymeConc))
 x <- 0:3
-png(filename="test1_1.png", width=750, height=750)
+png(filename="test1_1.png", width=1750, height=1550, res=300)
 par(mfrow=c(1,1))
 y2 <- c(y[1],log(y[y>0]))
-plot(x,y2, col=2, type="l", lwd=2, ylab='y', xlab='x',ylim=c(0,20),cex.axis=1.5)
+plot(x,y2, col=2, type="l", lwd=1, ylab='y', xlab='x',ylim=c(0,20),cex.axis=1)
 lm <- lm(y2~x)
-lines(x, predict(lm), col=3,lwd=2)
-lines(x,exp(x), col=4, lty= 2, lwd=2)
-points(y~x, pch=19)
+lines(x, predict(lm), col=3,lwd=1)
+lines(x,exp(x), col=4, lty= 2, lwd=1)
+points(y~x, pch=19,cex=0.7)
 legend("topleft", legend = c("Logarithmic transformation", "Predicted linear model", "Exponential transformation", "Raw data"),
-       col = c(2,3,4,1), lty=c(1,1,2,NA),lwd=3,pch=c(NA,NA,NA,19), cex=1.5)
+       col = c(2,3,4,1), lty=c(1,1,1,NA),lwd=1,pch=c(NA,NA,NA,19), cex=0.8)
 dev.off()
 
 # Transforming the data
@@ -68,11 +68,11 @@ cols3 <- c("black","red")
 col_bg3 <- adjustcolor(cols3, alpha = 0.2) 
 
 # Pairs plot
-png(filename="pairs_1.png", width=750, height=750)
+png(filename="pairs_1.png", width=1750, height=1750, res=300)
 pairs(data, col=as.numeric(data$Enzyme)+1, pch=19)
 dev.off()
 
-png(filename="bp_response_stock_1.png", width=750, height=750)
+png(filename="bp_response_stock_1.png", width=1750, height=1750, res=300)
 par(mfrow=c(1,2))
 plot(data$Response~data$DetStock, ylab="Response", xlab="Detergent",
      col=col_bg3, medcol=cols3, whiskcol=cols3, staplecol=cols3, boxcol=cols3, outcol=cols3,outbg=cols3)
@@ -80,25 +80,25 @@ plot(data$Response~data$CaStock, ylab="Response", xlab="Hardness",
      col=col_bg3, medcol=cols3, whiskcol=cols3, staplecol=cols3, boxcol=cols3, outcol=cols3,outbg=cols3)
 dev.off()
 
-png(filename="pairs_2.png", width=750, height=750)
+png(filename="pairs_2.png", width=1750, height=1750, res=300)
 pairs(df, col=as.numeric(df$Enzyme)+1, pch=19)
 dev.off()
 
 # Response  - Stock
-png(filename="bp_response_stock_2.png", width=750, height=750)
+png(filename="bp_response_stock_2.png", width=1750, height=1750, res=300)
 par(mfrow=c(1,1))
 boxplot(Response~Stock , data=df, xlab="Conditions (Detergent and Ca2++ combinations)", ylab="Protein removal (RU)", 
         col=col_bg3, medcol=cols3, whiskcol=cols3, staplecol=cols3, boxcol=cols3, outcol=cols3,outbg=cols3 )
 dev.off()
 
 # Response - Enzyme
-png(filename="bp_response_enzyme.png", width=750, height=750)
+png(filename="bp_response_enzyme.png", width=1750, height=1750, res=300)
 boxplot(Response~Enzyme, data=df, xlab="Enzyme type", ylab="Protein removal (RU)", 
         col=col_bg2, medcol=cols2, whiskcol=cols2, staplecol=cols2, boxcol=cols2, outcol=cols2, outbg=cols2)
 dev.off()
 
 # Response - concentration
-png(filename="bp_response_conc.png", width=750, height=750)
+png(filename="bp_response_conc.png",width=1750, height=1750, res=300)
 a <- boxplot(Response~EnzymeConc, data=data, xlab="Enzyme Concentration log(nM)", ylab="Protein removal (RU)", 
         col=col_bg, medcol=cols, whiskcol=cols, staplecol=cols, boxcol=cols, outcol=cols,outbg=cols,
         names=c(0,2.5, 7.5,15))
@@ -106,7 +106,7 @@ axis(side= 1, at=seq_along(a$names), tick = FALSE, labels = a$names)
 dev.off()
 
 # Response - Enzyme - Concentration
-png(filename="bp_response_enzyme_conc.png", width=750, height=750)
+png(filename="bp_response_enzyme_conc.png", width=1750, height=1750, res=300)
 par(mfrow = c(1,1))
 b <- boxplot(Response ~  EnzymeConc + Enzyme, data = data, xaxt = "n", xlab="Enzyme type",
              col= col_bg, medcol=cols, whiskcol=cols, staplecol=cols, boxcol=cols, outcol=cols,outbg=cols, 
@@ -115,7 +115,7 @@ axis(side= 1, at=seq_along(b$names), tick = FALSE, labels = b$names)
 legend("topright",title="Enzyme concentration", legend = c(0, 2.5, 7.5, 15), fill =cols, horiz =TRUE, cex=0.8)
 dev.off()
 
-png(filename="responseXconcentration.png", width=750, height=750)
+png(filename="responseXconcentration.png", width=1750, height=1750, res=300)
 par(mfrow=c(2,2))
 for (i in y2){
   plot(data$Response[data$EnzymeConc==i], pch=as.numeric(data$DetStock[data$EnzymeConc==i])+14, col=as.numeric(data$Enzyme[data$EnzymeConc==i]), ylab="Response", xlab="Observations", main=paste("Enzyme concentration: ",exp(i), "nM"))
@@ -235,7 +235,7 @@ BIC(lm1k, lm2i)
 
 
 # Outlier detection -------------------------------------------------------
-png(filename="outlier_detect.png", width=750, height=750)
+png(filename="outlier_detect.png",width=1750, height=1750, res=300)
 par(mfrow=c(1,1))
 qqPlot(lm1k)
 data <- data[-c(147,159),]
@@ -248,8 +248,44 @@ lm1k <- update(lm1k, ~.-data$DetStock:data$Enzyme:data$EnzymeConc)
 
 Anova(lm1k)
 summary(lm1k, correlation=TRUE)
-png(filename="LinearModel_Transformed.png", width=750, height=750)
+png(filename="LinearModel_Transformed.png",width=1750, height=1750, res=300)
 par(mfrow=c(2,2))
 plot(lm1k, col=as.numeric(data$Enzyme)+1, pch=19)
 dev.off()
+
+# Variance of the RunDate and the response --------------------------------
+# To see if the RunDate affects the performance of the experiments
+data <- read.table("~/Github/02441_Applied_Statistics/Case1/2_Data/SPR.txt", header = TRUE, sep="\t")
+
+
+day_var <- data[data$EnzymeConc == 0,]
+day_var <- day_var[,-c(2,4,6,7,8)]
+
+# Check the response distribution
+shapiro.test(day_var$Response)
+png(filename="distrib.png",  width=1850, height=700, res=300)
+par(mfrow=c(1,3), mar=c(4,4,1,1))
+x_range <- seq(min(day_var$Response),max(day_var$Response), length.out=100)
+hist1 <- hist(day_var$Response, prob=TRUE, freq = FALSE,
+              cex.axis=1,cex.lab=1, cex.main=1, main="Response distribution", xlab="Response")
+lines(x_range, dnorm(x_range, mean(day_var$Response), sd(day_var$Response)), 
+      col="red", lwd=1)
+plot(ecdf(day_var$Response), cex.axis=1,cex.lab=1,lwd=1,cex.main=1, main="Response ecdf")
+cdf <- pnorm(x_range, mean(day_var$Response), sd(day_var$Response))
+lines(x_range, cdf, col="red", type="l", lwd=1)
+qqPlot(day_var$Response,ylab="Response (RU)", lwd=1, cex=par(1), main="QQ Plot")
+dev.off()
+
+# Do Anova
+day_var$RunDate <- as.factor(day_var$RunDate)
+lm3 <- lm(day_var$Response ~ day_var$RunDate)
+Anova(lm3)
+png(filename="residuals_distrib_anova.png", width=1800, height=1800, res=300)
+par(mfrow=c(2,2))
+plot(lm3)
+dev.off()
+
+# Do Kruskal Wallis
+kruskal.test(day_var$Response ~ day_var$RunDate)
+# Not dignificant
 
