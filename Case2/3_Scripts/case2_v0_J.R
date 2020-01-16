d00f619523c2655b8f857f4630ef46814feb265a
jmontalvo94 <50923223+jmontalvo94@users.noreply.github.com>
2020-01-16 09:17:15 +0100

    Update case 0 J

--- Case2/3_Scripts/case2_v0_J.R
+++ Case2/3_Scripts/case2_v0_J.R
@@ -7,3 +7,17 @@
 
 
 # Load Data ---------------------------------------------------------------
+
+require("car")
+require("ggplot2")
+require("tidyverse")
+require("dplyr")
+require("xtable")
+require("rwunderground")
+
+# Load data and clean
+htk <- read_excel("~/Github/02441_Applied_Statistics/Case2/2_Data/HTK_building_data_share.xlsx")
+load("~/Github/02441_Applied_Statistics/Case2/2_Data/WUndergroundHourly.RData")
+
+files <- dir("~/Github/02441_Applied_Statistics/Case2/2_Data/meterdata", pattern="*.txt", full.names=TRUE)
+data <- history(set_location(lat_long="55.65,12.28"))
